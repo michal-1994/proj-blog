860927567a1ec80ad933d3862c81fa77bc26f696
Michal <michalgrzegorczyk04@gmail.com>
2023-11-05 11:28:31 +0100

    create contact form

---
+++ components/contact/contact-form.js
@@ -0,0 +1,30 @@
+import classes from './contact-form.module.css';
+
+function ContactForm() {
+    return (
+        <section className={classes.contact}>
+            <h1>How can I help you?</h1>
+            <form className={classes.form}>
+                <div className={classes.controls}>
+                    <div className={classes.control}>
+                        <label htmlFor="email">Your Email</label>
+                        <input type="email" id="email" />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="name">Your Name</label>
+                        <input type="text" id="name" />
+                    </div>
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="message">Your message</label>
+                    <textarea id="message" rows="5"></textarea>
+                </div>
+                <div className={classes.actions}>
+                    <button>Send Message</button>
+                </div>
+            </form>
+        </section>
+    );
+}
+
+export default ContactForm;
