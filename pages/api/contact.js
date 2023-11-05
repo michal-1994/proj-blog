2224fcc2254824f9d7e27e7f7a8de03ccbd020d4
Michal <michalgrzegorczyk04@gmail.com>
2023-11-05 12:38:13 +0100

    store messages with mongodb in a database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
 
@@ -20,7 +22,31 @@ function handler(req, res) {
             message
         };
 
-        console.log(newMessage);
+        let client;
+
+        try {
+            client = await MongoClient.connect(
+                `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b8gq0tj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+            );
+        } catch (error) {
+            res.status(500).json({ message: 'Could not connect to database.' });
+            return;
+        }
+
+        const db = client.db(process.env.DB_NAME);
+
+        try {
+            const result = await db
+                .collection('messages')
+                .insertOne(newMessage);
+            newMessage._id = result.insertedId;
+        } catch (error) {
+            client.close();
+            res.status(500).json({ message: 'Storing message failed.' });
+            return;
+        }
+
+        client.close();
 
         res.status(201).json({
             message: 'Successfully stored message!',
